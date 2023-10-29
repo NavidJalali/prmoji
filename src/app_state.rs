9251216ca1f77357c934f10f74303ac464f93b5e
NavidJalali <navid.jalali@moia.io>
2023-10-29 14:36:44 +0100

    fmt

--- src/app_state.rs
+++ src/app_state.rs
@@ -2,36 +2,36 @@ use crate::clock;
 use crate::database;
 
 pub trait AppState: Clone + Send + Sync + 'static {
-    type DB: database::Database + Sync + Send;
-    type Clock: clock::Clock + Sync + Send;
+  type DB: database::Database + Sync + Send;
+  type Clock: clock::Clock + Sync + Send;
 
-    fn db(&self) -> &Self::DB;
-    fn clock(&self) -> &Self::Clock;
+  fn db(&self) -> &Self::DB;
+  fn clock(&self) -> &Self::Clock;
 }
 
 #[derive(Clone)]
 pub struct TestState {
-    pub db: database::InMemoryDB,
-    pub clock: clock::LiveClock,
+  pub db: database::InMemoryDB,
+  pub clock: clock::LiveClock,
 }
 
 impl TestState {
-    pub fn new() -> Self {
-        Self {
-            db: database::InMemoryDB::new(),
-            clock: clock::LiveClock,
-        }
+  pub fn new() -> Self {
+    Self {
+      db: database::InMemoryDB::new(),
+      clock: clock::LiveClock,
     }
+  }
 }
 
 impl AppState for TestState {
-    type DB = database::InMemoryDB;
-    fn db(&self) -> &Self::DB {
-        &self.db
-    }
+  type DB = database::InMemoryDB;
+  fn db(&self) -> &Self::DB {
+    &self.db
+  }
 
-    type Clock = clock::LiveClock;
-    fn clock(&self) -> &Self::Clock {
-        &self.clock
-    }
+  type Clock = clock::LiveClock;
+  fn clock(&self) -> &Self::Clock {
+    &self.clock
+  }
 }
